5df419b1d4eb8057742f294730a2c8ac0a00238e
Daniel Bloch <79120202+danbloch98@users.noreply.github.com>
2025-06-21 19:39:34 -0700

    Add files via upload

---
+++ OS/kernel/arch/i386/tty.d
@@ -0,0 +1,8 @@
+arch/i386/tty.o: arch/i386/tty.c \
+ /root/opt/cross/lib/gcc/i686-elf/13.2.0/include/stdbool.h \
+ /root/opt/cross/lib/gcc/i686-elf/13.2.0/include/stddef.h \
+ /root/opt/cross/lib/gcc/i686-elf/13.2.0/include/stdint.h \
+ /root/opt/cross/lib/gcc/i686-elf/13.2.0/include/stdint-gcc.h \
+ /mnt/c/Users/daniel/OneDrive/Computation/OS/OS/sysroot/usr/include/string.h \
+ /mnt/c/Users/daniel/OneDrive/Computation/OS/OS/sysroot/usr/include/sys/cdefs.h \
+ include/kernel/tty.h arch/i386/vga.h
